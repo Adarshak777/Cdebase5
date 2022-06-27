3fa233f1149228e1802a7b8dafb68fb50961311e
Adarsh Kumar <adarsh.kumar@zensar.com>
2022-06-27 17:56:38 +0530

    Codebase5

---
+++ WebApplication7/Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication7.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }    
+        public string Branch { get; set; }
+    }
+}
